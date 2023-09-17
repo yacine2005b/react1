6c4cdf61b2a4bc6e640101784d486ec13f620a98
yacine bouziane <yybouz1802@gmail.com>
2023-09-17 20:42:39 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import Cards from './Components/Cards'
+import Cards from "./Components/Cards";
+import NavBar from "./Components/NavBar";
 
-import {data} from './data'
-export default function App(){
-    const datashow =data.map((item,index) => <Cards 
-        key ={index}
-        img={item.img} 
-         title={item.title} 
-        desc={item.desc} 
-        review={item.reveiw} 
-        price={item.price}
-        date ={item.date}/>
-        )
+import { data } from "./data";
+export default function App() {
+  const datashow = data.map((item, index) => (
+    <Cards
+      key={index}
+      img={item.img}
+      title={item.title}
+      desc={item.desc}
+      review={item.reveiw}
+      price={item.price}
+      date={item.date}
+    />
+  ));
 
-    return <div className='cards'>
+  return (
+    <div className="cards">
+      <NavBar />
       {datashow}
-      </div>
+    </div>
+  );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">places</a>
+        </li>
+        <li>
+          <a href="#">other services</a>
+        </li>
+        <li>
+          <a href="#">About us</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
